00ac981b8b86a31be5d835f5ff0f68748435a476
zhengfang <215566435@qq.com>
2018-08-28 13:10:09 +0800

    chore: fix not change store bugs

--- src/__test__/index.test.jsx
+++ src/__test__/index.test.jsx
@@ -8,30 +8,40 @@ import {init} from '..';
 configure({adapter: new Adapter()});
 
 describe('test for rectx', () => {
-  const {Put, Ctx, Val, Auto} = init({
-    value: 1,
-    dummy: 1,
-  });
-
-  const TestComponent = () => Ctx(s => <div id="test-div">{s.value}</div>);
-
   it('renders without crashing', () => {
+    const {Ctx} = init({
+      value: 1,
+      dummy: 1,
+    });
+
+    const TestComponent = () => Ctx(s => <div id="test-div">{s.value}</div>);
     const wrapper = mount(<TestComponent />);
     const node = wrapper.find('#test-div');
     expect(node.getDOMNode().textContent).equal('1');
   });
 
   it('should mutate when calling `Put`', async () => {
+    const {Put, Ctx} = init({
+      value: 1,
+      dummy: 1,
+    });
+
+    const TestComponent = () => Ctx(s => <div id="test-div">{s.value}</div>);
     const wrapper = mount(<TestComponent />);
     const node = wrapper.find('#test-div');
     expect(node.getDOMNode().textContent).equal('1');
 
-    Put({value: 2});
+    Put(s => (s.value = 2));
 
     expect(node.getDOMNode().textContent).equal('2');
   });
 
   it('should mutate when use `mutatable api`', async () => {
+    const {Put, Ctx} = init({
+      value: 1,
+      dummy: 1,
+    });
+    const TestComponent = () => Ctx(s => <div id="test-div">{s.value}</div>);
     const wrapper = mount(<TestComponent />);
     const node = wrapper.find('#test-div');
     expect(node.getDOMNode().textContent).equal('1');
@@ -42,19 +52,31 @@ describe('test for rectx', () => {
   });
 
   it('should unListen when component is unmounted && without getting wrong', async () => {
+    const Store = init({
+      value: 1,
+      dummy: 1,
+    });
+    const TestComponent = () =>
+      Store.Ctx(s => <div id="test-div">{s.value}</div>);
+
     const wrapper = mount(<TestComponent />);
     const node = wrapper.find('#test-div');
     expect(node.getDOMNode().textContent).equal('1');
 
     wrapper.unmount();
-    Put(s => (s.value = 2));
+    Store.Put(s => (s.value = 2));
 
-    Ctx(s => {
-      expect(s.value).equal('2');
-    });
+    // expect(node.getDOMNode().textContent).equal('2');
+    expect(Store._store().value).equal(2);
+    Store.Put(s => (s.value = 10));
+    expect(Store._store().value).equal(10);
   });
 
   test('`Ctx` can be a render-props funtion', async () => {
+    const {Put, Ctx} = init({
+      value: 1,
+      dummy: 1,
+    });
     const App = () => (
       <Ctx>{s => <div id="test-div-render-props">{s.value}</div>}</Ctx>
     );
@@ -157,13 +179,17 @@ describe('test for rectx', () => {
   // });
 
   test('`Auto` and `Ctx` testting ', async () => {
+    const {Put, Ctx, Auto} = init({
+      value: 1,
+      dummy: 1,
+    });
+
     let renderTimes = 0;
     const Dumb = Auto(s => s.dummy);
     const Dummy = ({data}) => {
       renderTimes = renderTimes + 1;
       return <div id="dummy">{data}</div>;
     };
-
 
     const App = () => (
       <div>
--- src/index.js
+++ src/index.js
@@ -11,20 +11,31 @@ class Wrapper extends React.Component {
   }
 }
 
-function createSubscribe() {
-  const listener = [];
-  return {
-    listen: updator => listener.push(updator),
-    unListen: l => listener.splice(listener.indexOf(l), 1),
-    update: updator => {
-      for (let i = 0; i < listener.length; i++) {
-        listener[i](updator);
-      }
-    },
-  };
-}
-
-export function init(store) {
+export function init(_store) {
+  let store = _store;
+  function createSubscribe() {
+    const listener = [];
+    return {
+      listen: updator => listener.push(updator),
+      unListen: l => listener.splice(listener.indexOf(l), 1),
+      update: updator => {
+        for (let i = 0; i < listener.length; i++) {
+          listener[i](updator);
+        }
+        if (listener.length === 0) {
+          if (typeof updator === 'function') {
+            const newState = produce(store, draft => {
+              updator(draft);
+            });
+            if (!equal(newState, store)) {
+              store = newState;
+            }
+            return;
+          }
+        }
+      },
+    };
+  }
   const {listen, unListen, update} = createSubscribe();
 
   class Provider extends React.PureComponent {
@@ -40,12 +51,9 @@ export function init(store) {
         });
         if (!equal(newState, this.state)) {
           this.setState(newState);
+          store = newState;
         }
         return;
-      }
-
-      if (!equal(updator, this.state)) {
-        this.setState(updator);
       }
     };
     componentWillUnmount() {
@@ -62,6 +70,7 @@ export function init(store) {
   }
 
   return {
+    _store: () => store,
     Ctx: props => {
       if (typeof props !== 'function') {
         return <Provider>{props.children}</Provider>;
